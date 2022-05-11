294662c91ffc69ffb2d738d0e189d76bcc3a3e31
carloscoronad0 <55253916+carloscoronad0@users.noreply.github.com>
2022-05-11 16:43:29 -0400

    Previous errors corrected

---
+++ dentistDBCreation.sql
@@ -0,0 +1,43 @@
+use mydentistdatabase
+go
+
+CREATE TABLE Dentists (
+	id INT NOT NULL AUTO_INCREMENT,
+	dentist_name VARCHAR(100),
+	speciality VARCHAR(100),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Clients (
+	id 				INT NOT NULL AUTO_INCREMENT,
+	client_name 	VARCHAR(100),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Appointments (
+	id INT NOT NULL AUTO_INCREMENT,
+	dentist_id INT NOT NULL,
+	client_id INT NOT NULL,
+	appointment_date VARCHAR(100),
+	appointment_hour VARCHAR(10),
+	PRIMARY KEY (id),
+	CONSTRAINT FK_Appointment_DentistId FOREIGN KEY (dentist_id) REFERENCES Dentists(id),
+	CONSTRAINT FK_Appointment_ClientId FOREIGN KEY (client_id) REFERENCES Clients(id)
+);
+
+CREATE TABLE Payments (
+	id INT NOT NULL AUTO_INCREMENT,
+	appointment_id INT NOT NULL,
+	amount INT,
+	method VARCHAR(20),
+	PRIMARY KEY (id),
+	CONSTRAINT FK_Payment_AppointmentId FOREIGN KEY (appointment_id) REFERENCES Appointments(id)
+);
+
+CREATE TABLE Canceled (
+	id INT NOT NULL AUTO_INCREMENT,
+	appointment_id INT NOT NULL,
+	reason VARCHAR(200),
+	PRIMARY KEY (id),
+	CONSTRAINT FK_Canceled_AppointmentId FOREIGN KEY (appointment_id) REFERENCES Appointments(id)
+);
