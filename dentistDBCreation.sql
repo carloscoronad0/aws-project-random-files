9493f3724af8276e9f04311befb84a8d7c964e88
carloscoronad0 <55253916+carloscoronad0@users.noreply.github.com>
2022-05-12 13:53:39 -0400

    Changed com table names

--- dentistDBCreation.sql
+++ dentistDBCreation.sql
@@ -5,12 +5,16 @@ CREATE TABLE Dentists (
 	id INT NOT NULL AUTO_INCREMENT,
 	dentist_name VARCHAR(100),
 	speciality VARCHAR(100),
+	createdAt DATE,
+	updatedAt DATE,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE Clients (
 	id INT NOT NULL AUTO_INCREMENT,
 	client_name VARCHAR(100),
+	createdAt DATE,
+	updatedAt DATE,
 	PRIMARY KEY (id)
 );
 
@@ -20,6 +24,8 @@ CREATE TABLE Appointments (
 	client_id INT NOT NULL,
 	appointment_date VARCHAR(100),
 	appointment_hour INT,
+	createdAt DATE,
+	updatedAt DATE,
 	PRIMARY KEY (id),
 	CONSTRAINT FK_Appointment_DentistId FOREIGN KEY (dentist_id) REFERENCES Dentists(id),
 	CONSTRAINT FK_Appointment_ClientId FOREIGN KEY (client_id) REFERENCES Clients(id)
@@ -30,14 +36,18 @@ CREATE TABLE Payments (
 	appointment_id INT NOT NULL,
 	amount INT,
 	method VARCHAR(20),
+	createdAt DATE,
+	updatedAt DATE,
 	PRIMARY KEY (id),
 	CONSTRAINT FK_Payment_AppointmentId FOREIGN KEY (appointment_id) REFERENCES Appointments(id)
 );
 
-CREATE TABLE Canceled (
+CREATE TABLE Cancellations (
 	id INT NOT NULL AUTO_INCREMENT,
 	appointment_id INT NOT NULL,
 	reason VARCHAR(200),
+	createdAt DATE,
+	updatedAt DATE,
 	PRIMARY KEY (id),
 	CONSTRAINT FK_Canceled_AppointmentId FOREIGN KEY (appointment_id) REFERENCES Appointments(id)
 );
