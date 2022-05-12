15894218d0aae6bb7d9524b23c8630c785887668
carloscoronad0 <55253916+carloscoronad0@users.noreply.github.com>
2022-05-12 14:12:16 -0400

    Dates format changed for param use

--- dentistDBInsert.sql
+++ dentistDBInsert.sql
@@ -12,9 +12,9 @@ INSERT INTO Clients (client_name)
     ("Boris Pedraza");
 
 INSERT INTO Appointments (dentist_id, client_id, appointment_date, appointment_hour)
-    VALUES (1, 2, "13/04/22", 15),
-    (2, 3, "25/02/22", 14),
-    (3, 2, "01/03/22", 11);
+    VALUES (1, 2, "13-04-22", 15),
+    (2, 3, "25-02-22", 14),
+    (3, 2, "01-03-22", 11);
 
 INSERT INTO Payments (appointment_id, amount, method)
     VALUES (1, 200, "Efectivo"),
